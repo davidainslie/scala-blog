cbe77494f5bbd38129535c312c188fe2f4800fe6
David Ainslie <dainslie@gmail.com>
2016-07-07 09:49:15 +0100

    Rearranged some code

--- src/main/scala/com/kissthinker/trading/package.scala
+++ src/main/scala/com/kissthinker/trading/package.scala
@@ -1,23 +1,6 @@
 package com.kissthinker
 
-import scala.concurrent.ExecutionContext.Implicits.global
-import scala.concurrent.{Future, Promise}
-import scala.util.Try
-
 package object trading {
-  implicit class FutureOps[T](val self: Future[T]) {
-    def or(other: Future[T]): Future[T] = {
-      val p = Promise[T]()
-
-      val completePromise = (t: Try[T]) => p tryComplete t
-
-      self onComplete completePromise
-      other onComplete completePromise
-
-      p.future
-    }
-  }
-
   def bestBuy(atLeastOneQuote: Quote, quotes: Quote*) = quotes.foldLeft(atLeastOneQuote) { (best, quote) =>
     if (best.price <= quote.price) best else quote
   }
